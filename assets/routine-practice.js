65a6500bf1267a1dad4db26fdf96386f6c098bf2
eduardoserranow-oss <eduardoserranow@gmail.com>
2026-08-22 23:19:03 -0400

    Bump routine cache refresh

--- assets/routine-practice.js
+++ assets/routine-practice.js
@@ -277,7 +277,7 @@
 
   function redirectToLogin() {
     const loginUrl = new URL("login.html", window.location.href);
-    loginUrl.searchParams.set("v", "routinepicker2");
+    loginUrl.searchParams.set("v", "cachefix3");
     const currentPath = window.location.pathname + window.location.search + window.location.hash;
     loginUrl.searchParams.set("returnTo", currentPath);
     window.location.replace(loginUrl.href);
