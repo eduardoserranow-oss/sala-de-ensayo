f449f9bb637041634e9317791970bf6f4ec0f2d4
eduardoserranow-oss <eduardoserranow@gmail.com>
2026-08-28 03:28:07 -0400

    Add compact Afro drum web bank map

---
+++ assets/vibe-roulette-afro-drum-bank-v1.js
@@ -0,0 +1,33 @@
+export const AFRO_DRUM_PACK_BASE='./assets/vibe-roulette/drums/afro/web';
+export const AFRO_DRUM_BANK={
+  'vr-afro-drum-001':{pack:1,start:0.000000000,duration:19.200000000},
+  'vr-afro-drum-002':{pack:1,start:19.450000000,duration:8.421043084},
+  'vr-afro-drum-003':{pack:1,start:28.121043084,duration:9.795918367},
+  'vr-afro-drum-004':{pack:1,start:38.166961451,duration:17.142857143},
+  'vr-afro-drum-005':{pack:2,start:0.000000000,duration:9.411768707},
+  'vr-afro-drum-006':{pack:2,start:9.661768707,duration:15.737709751},
+  'vr-afro-drum-007':{pack:2,start:25.649478458,duration:16.000000000},
+  'vr-afro-drum-008':{pack:2,start:41.899478458,duration:20.210521542},
+  'vr-afro-drum-009':{pack:3,start:0.000000000,duration:16.410249433},
+  'vr-afro-drum-010':{pack:3,start:16.660249433,duration:16.271179138},
+  'vr-afro-drum-011':{pack:3,start:33.181428571,duration:17.454557823},
+  'vr-afro-drum-012':{pack:3,start:50.885986395,duration:16.551723356},
+  'vr-afro-drum-013':{pack:4,start:0.000000000,duration:17.142857143},
+  'vr-afro-drum-014':{pack:4,start:17.392857143,duration:16.271179138},
+  'vr-afro-drum-015':{pack:4,start:33.914036281,duration:9.795918367},
+  'vr-afro-drum-016':{pack:4,start:43.959954649,duration:16.000000000},
+  'vr-afro-drum-017':{pack:5,start:0.000000000,duration:17.142857143},
+  'vr-afro-drum-018':{pack:5,start:17.392857143,duration:15.360000000},
+  'vr-afro-drum-019':{pack:5,start:33.002857143,duration:16.271179138},
+  'vr-afro-drum-020':{pack:5,start:49.524036281,duration:15.867777778},
+  'vr-afro-drum-021':{pack:6,start:0.000000000,duration:16.991133787},
+  'vr-afro-drum-022':{pack:6,start:17.241133787,duration:19.591836735},
+  'vr-afro-drum-023':{pack:6,start:37.082970522,duration:16.695668934},
+  'vr-afro-drum-024':{pack:6,start:54.028639456,duration:15.360000000},
+  'vr-afro-drum-025':{pack:7,start:0.000000000,duration:15.483877551},
+  'vr-afro-drum-026':{pack:7,start:15.733877551,duration:8.727278912},
+  'vr-afro-drum-027':{pack:7,start:24.711156463,duration:16.695668934},
+  'vr-afro-drum-028':{pack:7,start:41.656825397,duration:9.600000000}
+};
+export function afroDrumPackPath(pack){return `${AFRO_DRUM_PACK_BASE}/vr-afro-pack-${pack}.m4a`;}
+export const AFRO_DRUM_BANK_INFO={version:1,packs:7,codec:'AAC-LC',sampleRateHz:44100,source:'user-supplied WAV masters',purpose:'compact web playback bank; original filenames remain visible in UI'};
