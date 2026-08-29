e0d00a5accae4412290099118a252ea3b9d4f159
eduardoserranow-oss <eduardoserranow@gmail.com>
2026-08-29 16:07:43 -0400

    Package Windows icon resources for native MIDI drag

--- desktop/forge.config.js
+++ desktop/forge.config.js
@@ -1,7 +1,14 @@
+const path = require('node:path');
+
+const windowsIcon = path.resolve(__dirname, '..', 'assets', 'forte-favicon.ico');
+const dragIcon = path.resolve(__dirname, '..', 'assets', 'favicon.png');
+
 module.exports = {
   packagerConfig: {
     asar: true,
-    executableName: 'FORTISSIMO'
+    executableName: 'FORTISSIMO',
+    icon: windowsIcon,
+    extraResource: [dragIcon]
   },
   rebuildConfig: {},
   makers: [
@@ -10,6 +17,7 @@ module.exports = {
       config: {
         name: 'fortissimo_desktop',
         setupExe: 'FORTISSIMO-Setup.exe',
+        setupIcon: windowsIcon,
         noMsi: true
       }
     }
